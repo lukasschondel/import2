329ca9c0df443514c282ae45e75bce3b543967b9
lukasschondel <94137003+lukasschondel@users.noreply.github.com>
2023-07-12 14:04:15 +0200

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vitepress'
+  import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/YOUR_REPOSITORY_NAME/",  
+  base: "/import2/",  
   title: "Simple VitePress Site",
   description: "Training Page for Cloud Computing Course",
-...
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
